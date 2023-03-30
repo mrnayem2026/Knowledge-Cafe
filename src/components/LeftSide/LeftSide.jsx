dd8fcb98966d1de2a0d9ac8919cdb21f92f10da9
Mostafizur Rahman Nayem <hpbd17062022@gmail.com>
2023-03-30 23:14:10 +0600

    left side bar added

--- src/components/LeftSide/LeftSide.jsx
+++ src/components/LeftSide/LeftSide.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 const LeftSide = () => {
     return (
-        <div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis vero suscipit voluptates aspernatur corporis tempora provident numquam? Iure iusto vitae error vel eum numquam officiis optio accusantium quas sint corporis praesentium inventore odit, suscipit est odio, quo minus eos ab distinctio amet reiciendis? Similique quidem praesentium tempora corrupti quaerat dolores veniam sit enim consequuntur exercitationem.
+        <div className=' sticky top-0 bg-cyan-600 rounded-lg p-10 ml-4 text-black'>
+            <h1 className='text-2xl font-black'>Spent time on read : 0 min</h1>
+            <h1 className='text-xl font-medium mt-3'>Bookmarked Blogs : 0</h1>
         </div>
     );
 };
